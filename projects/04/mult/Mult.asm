db055ebdc9491c664efaaf0084cca5be87f50c07
Kelton Manzanares <kmanzana@mines.edu>
2014-02-05 21:25:43 -0700

    Update Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -15,6 +15,8 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
+// Use Russian multiplication
+
 // Pseudo code:
 // R2 = 0
 // do {
