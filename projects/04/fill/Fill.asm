596fe0296d1a70c40d55946b5e45043f99814555
Kelton Manzanares <kmanzana@mines.edu>
2014-02-07 10:52:26 -0700

    finished ECS4A

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -17,5 +17,67 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+  @8192
+  D=A
+  @size
+  M=D // size = 8192
 
+  @write
+  M=-1 // write = 0
+
+(MAIN)
+
+  @KBD
+  D=M
+  @PRESSED
+  D;JNE
+  @write
+  M=0
+  @NOT_PRESSED
+  0;JMP
+(PRESSED)
+  @write
+  M=-1
+(NOT_PRESSED)
+  @index
+  M=0 // index = 0
+
+(LOOP)
+  @index
+  D=M
+  @SCREEN
+  D=A+D
+  @pointer
+  M=D
+  @write
+  D=M
+  @pointer
+  A=M
+  M=D
+  @index
+  M=M+1
+  D=M
+  @size
+  D=D-M
+  @LOOP
+  D;JNE
+
+  @MAIN
+  0;JMP
+// Pseudo code
+// size = 2^13 = 8192
+// while true
+//   if KBD == 0
+//     a = size
+//     while a != 0
+//       SCREEN[a] = -1
+//       a--
+//     end
+//   else
+//     a = size
+//     while a != 0
+//       SCREEN[a] = 0
+//       a--
+//     end
+//   end
+// end
