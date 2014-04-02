1bcaf3a4a5449f498f1592742664a86ee9b45b85
Kelton Manzanares <kmanzana@mines.edu>
2014-04-02 17:50:36 -0600

    working!

---
+++ projects/10A/JackAnalyzer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#============================================================
+# USERID:......... KMANZANA
+# PROGRAMMER:..... Manzanares, Kelton M.
+# COURSE:......... CSCI-410
+# TERM:........... SP14
+# PROJECT:........ ECS10A
+# FILENAME:....... JackAnalyzer.py
+# PYTHON VERSION:. 2.7.2
+#============================================================
+
+from main import Main
+
+Main().main()
---
+++ projects/10A/JackTokenizer.py
@@ -0,0 +1,10 @@
+#============================================================
+# USERID:......... KMANZANA
+# PROGRAMMER:..... Manzanares, Kelton M.
+# COURSE:......... CSCI-410
+# TERM:........... SP14
+# PROJECT:........ ECS10A
+# FILENAME:....... JackTokenizer.py
+# PYTHON VERSION:. 2.7.2
+#============================================================
+
