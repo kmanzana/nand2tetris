c7766600b6efe4a29b9e876e73f0df87179acad7
Kelton Manzanares <kmanzana@mines.edu>
2014-04-20 17:49:54 -0600

    symboltable working

--- projects/11/SymbolTable.py
+++ projects/11/SymbolTable.py
@@ -10,26 +10,66 @@
 
 class SymbolTable:
   def __init__(self):
-    pass
+    self.counts = {
+      'STATIC': 0,
+      'FIELD':  0,
+      'ARG':    0,
+      'VAR':    0
+    }
+
+    self.subroutine_scope = {}
+    self.class_scope      = {}
 
   def startSubroutine(self):
-    pass
+    self.subroutine_scope = {}
+    self.counts['ARG']    = 0
+    self.counts['VAR']    = 0
 
   def define(self, name, type, kind):
-    pass
+    number = None
+
+    if kind == 'ARG' or kind == 'VAR':
+      if kind == 'ARG':
+        self.counts['ARG'] += 1
+        number = self.counts['ARG']
+      elif kind == 'VAR':
+        self.counts['VAR'] += 1
+        number = self.counts['VAR']
+
+      self.subroutine_scope[name] = (type, kind, number)
+    elif kind == 'STATIC' or kind == 'FIELD':
+      if kind == 'STATIC':
+        self.counts['STATIC'] += 1
+        number = self.counts['STATIC']
+      elif kind == 'FIELD':
+        self.counts['FIELD'] += 1
+        number = self.counts['FIELD']
+
+      self.class_scope[name] = (type, kind, number)
 
   def varCount(self, kind):
-    pass
-    return int
+    return self.counts[kind]
 
   def kindOf(self, name):
-    pass
-    return (STATIC, FIELD, ARG, VAR, NONE)
+    if name in self.subroutine_scope.keys():
+      return self.subroutine_scope[name][1]
+    elif name in self.class_scope.keys():
+      return self.class_scope[name][1]
+    else:
+      return 'NONE'
 
   def typeOf(self, name):
-    pass
-    return string
+    if name in self.subroutine_scope.keys():
+      return self.subroutine_scope[name][0]
+    elif name in self.class_scope.keys():
+      return self.class_scope[name][0]
+    else:
+      return 'NONE'
 
   def indexOf(self, name):
-    pass
-    return int
+    if name in self.subroutine_scope.keys():
+      return self.subroutine_scope[name][2]
+    elif name in self.class_scope.keys():
+      return self.class_scope[name][2]
+    else:
+      return'NONE'
---
+++ projects/11/SymbolTable_Spec.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import unittest
+from SymbolTable import SymbolTable
+
+class TestSymbolTable(unittest.TestCase):
+  def setUp(self):
+    self.symbol_table = SymbolTable()
+
+  def tearDown(self):
+    pass
+
+  # def assertion(self, actual, expected):
+  #   asserted = actual == expected
+  #   if not asserted:
+  #     print 'FAILED: assert {} == {}'.format(actual, expected)
+  #   assert asserted
+
+  def test_constructor(self):
+    assert self.symbol_table.__class__.__name__ == 'SymbolTable'
+
+  def test_define_VAR(self):
+    self.symbol_table.define('foo', 'int', 'STATIC')
+
+    assert self.symbol_table.counts['STATIC'] == 1
+    assert self.symbol_table.class_scope['foo'] == ('int', 'STATIC', 1)
+
+  def test_define_var(self):
+    self.symbol_table.define('bar', 'Snake', 'VAR')
+
+    assert self.symbol_table.counts['VAR'] == 1
+    assert self.symbol_table.subroutine_scope['bar'] == ('Snake', 'VAR', 1)
+
+  def test_varCount(self):
+    self.symbol_table.define('foo', 'int', 'FIELD')
+
+    assert self.symbol_table.varCount('FIELD')  == 1
+    assert self.symbol_table.varCount('ARG')    == 0
+    assert self.symbol_table.varCount('STATIC') == 0
+    assert self.symbol_table.varCount('VAR')    == 0
+
+  def test_kindOf(self):
+    self.symbol_table.define('foo', 'int', 'FIELD')
+
+    assert self.symbol_table.kindOf('foo') == 'FIELD'
+    assert self.symbol_table.kindOf('bar') == 'NONE'
+
+  def test_typeOf(self):
+    self.symbol_table.define('foo', 'Square', 'FIELD')
+
+    assert self.symbol_table.typeOf('foo') == 'Square'
+    assert self.symbol_table.typeOf('bar') == 'NONE'
+
+  def test_indexOf(self):
+    self.symbol_table.define('foo', 'boolean', 'FIELD')
+    self.symbol_table.define('bar', 'char', 'FIELD')
+    self.symbol_table.define('qaz', 'char', 'STATIC')
+
+    assert self.symbol_table.indexOf('foo') == 1
+    assert self.symbol_table.indexOf('bar') == 2
+    assert self.symbol_table.indexOf('qaz') == 1
+    assert self.symbol_table.indexOf('quz') == 'NONE'
+
+unittest.main()
